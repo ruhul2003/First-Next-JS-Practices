e8958cb41cf48cce7c242845ada09c9303407e28
ruhul2003 <ruhul941020@gmail.com>
2026-04-10 02:23:36 +0600

    Dynamic route

---
+++ src/app/blogs/[blogId]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h1 className="text-5xl font-bold text-center mt-30">Blog is coming soon</h1>
+        </div>
+    );
+};
+
+export default page;
