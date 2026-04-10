1830a3c850e9afc25949c764b615a198f09f384f
ruhul2003 <ruhul941020@gmail.com>
2026-04-11 01:39:36 +0600

    Active nav

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -1,11 +1,17 @@
+'use client';
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 
 const Navbar = () => {
 
+    const pathName = usePathname();
+    console.log(pathName);
+
     const links = <>
-        <li><Link href="/about">About</Link></li>
-        <li><Link href="/contact">Contact</Link></li>
-        <li><Link href="/blogs">Blogs</Link></li>
+        <li><Link className={pathName=== '/about' ? "text-blue-500 font-bold" : ""} href="/about">About</Link></li>
+        <li><Link className={pathName=== '/contact' ? "text-blue-500 font-bold" : ""} href="/contact">Contact</Link></li>
+        <li><Link className={pathName=== '/blogs' ? "text-blue-500 font-bold" : ""} href="/blogs">Blogs</Link></li>
         
     </>
     return (
